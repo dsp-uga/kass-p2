74a3738c01600413e4f5ea49046a698c86423e78
YDivyaKrishna <divyakrishna0497@gmail.com>
2021-03-18 00:27:10 -0400

    Logistic Regression

---
+++ image_classifier.py
@@ -0,0 +1,36 @@
+mport sys
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql import SparkSession
+from pyspark.sql.types import DoubleType
+from pyspark.sql.functions import col
+from pyspark.ml.classification import LogisticRegression
+spark = SparkSession.builder.appName('image_classifier').ge
+tOrCreate()
+x_train_file= sys.argv[1]
+x_test_file = sys.argv[2]
+output_path = "/home/dsp_kass/outputs/Logistic_Reg" #sys.ar
+gv[3]
+train_df = spark.read.csv(x_train_file, header=True, inferS
+chema = True)
+         
+feature_columns = train_df.columns[9:-1]
+assembler = VectorAssembler(inputCols= feature_columns, out
+putCol = "features")
+train = assembler.transform(train_df)
+             
+      
+test_df = spark.read.csv(x_test_file, header = True, inferS
+chema=True) 
+test_assembler = VectorAssembler(inputCols=test_df.columns[
+9:], outputCol="features")
+test = test_assembler.transform(test_df)
+    
+algo = LogisticRegression(featuresCol = "features", labelCo
+l="Sex (subj)")
+    
+model = algo.fit(train)
+    
+predictions = model.transform(test) 
+predictions.select('prediction').coalesce(1).rdd.map(lambda
+ x: int(x[0])).saveAsTextFile(output_path)
+print('predictions are stored in the output file')
